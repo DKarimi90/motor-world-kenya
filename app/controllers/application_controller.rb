16cb7a4b970457edbbfcd7df48bc00d377da1604
nthokiesther09 <nthokiesther09@gmail.com>
2023-04-04 15:38:00 +0300

    updated file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    before_action :set_headers
+    def set_headers
+        headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      end
 end
