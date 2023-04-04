84fcf4a7e590f303de518fccdc1cc18c21b58cb2
nthokiesther09 <nthokiesther09@gmail.com>
2023-04-04 14:15:18 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :show_rooms
+    resources :users, only: [:index, :create]
+    resources :reviews, only: [:index, :show, :create, :destroy]
+    resources :cars, only: [:index, :show, :create, :update, :destroy]
+   #register new user
+    post "/users/signup", to: "users#signup"
+    post "/users/login", to: "users#login"
+    delete "/users/logout", to: "users#destroy"
+    post "sessions/signup", to: "sessions#signup"
+   
+    post '/reviews/create', to: 'reviews#create'
+    delete '/reviews/delete', to: 'reviews#destroy'
+    post '/cars/create', to: 'cars#create'
 end
